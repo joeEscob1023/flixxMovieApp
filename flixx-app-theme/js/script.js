b35f235091aa7bd29f472c8da559d26b00a84f37
Joe Escobedo <joe_escob1023@live.com>
2023-06-12 15:09:13 -0500

    add movie details

--- flixx-app-theme/js/script.js
+++ flixx-app-theme/js/script.js
@@ -72,6 +72,77 @@ async function displayPopularShows() {
   });
 }
 
+//Display movie details
+
+async function displayMovieDetails() {
+  const movieId = window.location.search.split('=')[1];
+
+  const movie = await fetchAPIData(`movie/${movieId}`);
+
+  const div = document.createElement('div');
+
+  div.innerHTML = `
+  <div class="details-top">
+  <div>
+  ${
+    movie.poster_path
+      ? ` <img
+   src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+   class="card-img-top"
+   alt="${movie.title}"
+ />`
+      : ` <img
+ src="images/no-image.jpg"
+ class="card-img-top"
+ alt="${movie.title}"
+/>`
+  }/>
+  </div>
+  <div>
+    <h2>${movie.title}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${movie.vote_average.toFixed(1)} / 10
+    </p>
+    <p class="text-muted">Release Date: ${movie.release_date}</p>
+    <p>
+     ${movie.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+      ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+    </ul>
+    <a href="${
+      movie.homepage
+    }" target="_blank" class="btn">Visit Movie Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Movie Info</h2>
+  <ul>
+    <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
+      movie.budget
+    )}</li>
+    <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
+      movie.revenue
+    )}</li>
+    <li><span class="text-secondary">Runtime:</span> ${
+      movie.runtime
+    } minutes</li>
+    <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">
+    ${movie.production_companies
+      .map((company) => `<span>${company.name}</span>`)
+      .join('')}
+  </div>
+</div>
+  `;
+
+  document.querySelector('#movie-details').appendChild(div);
+}
+
 //Fetch data from TMDB API
 //normally you wouldn't make the API public
 async function fetchAPIData(endpoint) {
@@ -108,6 +179,10 @@ function highlightActiveLink() {
   });
 }
 
+function addCommasToNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 //Init App
 function init() {
   //Have to add this variable because I messed up by putting the flixx-app-theme inside another folder instead of making it the root folder. I already have it git and I dont want to mess up the app and start all over again. I probably could fix it, but its not really important right now.
@@ -120,7 +195,7 @@ function init() {
       displayPopularShows();
       break;
     case `${root}/movie-details.html`:
-      console.log('movie details');
+      displayMovieDetails();
       break;
     case `${root}/tv-details.html`:
       console.log('tv details');
